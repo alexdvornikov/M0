49fed6d8c38aee15fb9a6595bb20ad4991af9b69
Daniel Douglas <dougl215@msu.edu>
2022-04-26 15:04:12 -0400

    added PCA-based method

--- track_closeness.py
+++ track_closeness.py
@@ -4,6 +4,19 @@ import h5py
 
 vd = 1.6
 
+class POCA:
+    def __init__(self, d, pointA, pointB):
+        """
+        This class contains data about a point of closest approach (POCA)
+        d: distance of closest approach
+        pointA: point from one track for which distance is minimized
+        pointB: point from other track for which distance is minimized
+        """
+
+        self.d = d
+        self.pointA = pointA
+        self.pointB = pointB
+
 def pairWiseDist(posPairArray):
     return np.sqrt(np.sum(np.power(posPairArray[:,1,:] - posPairArray[:,0,:], 2), axis = -1))
 
@@ -15,6 +28,8 @@ def closeness(trackA, trackB, h5File, metric = 'hit'):
         t0A = trackA['t0']
         t0B = trackB['t0']
 
+        print (hitsA['ts'])
+        print (hitsA['ts'] - t0A)
         posA = np.array([hitsA['px'],
                          hitsA['py'],
                          vd*(hitsA['ts'] - t0A)])
@@ -25,12 +40,55 @@ def closeness(trackA, trackB, h5File, metric = 'hit'):
         posPairs = np.array([[thisPosA, thisPosB]
                              for thisPosA in posA.T
                              for thisPosB in posB.T])
-        return np.min(pairWiseDist(posPairs))
+        pwd = pairWiseDist(posPairs)
+        d = np.min(pwd)
+        posPairMin = np.argmin(pwd)
+        posAMin, posBMin = posPairs[posPairMin]
+        print (posAMin, posBMin)
+        return (d)
 
     elif metric == 'PCA':
         # TODO implement segment-based distance
-        return 0
-    
+        As = np.array([trackA['start'][0],
+                       trackA['start'][1],
+                       trackA['start'][2]])
+        Ae = np.array([trackA['end'][0],
+                       trackA['end'][1],
+                       trackA['end'][2]])
+        Ahat = As - Ae
+        
+        Bs = np.array([trackB['start'][0],
+                       trackB['start'][1],
+                       trackB['start'][2]])
+        Be = np.array([trackB['end'][0],
+                       trackB['end'][1],
+                       trackB['end'][2]])
+        Bhat = Bs - Be
+                
+        det = np.dot(Ahat, Ahat)*np.dot(Bhat, Bhat) - np.power(np.dot(Ahat, Bhat), 2)
+
+        # length along the 'A' track.  Within the track is l in [0, 1]
+        l = 1./det*(np.dot(Bhat, Bhat)*(np.dot(Be, Ahat) - np.dot(Ae, Ahat)) + np.dot(Ahat, Bhat)*(np.dot(Ae, Bhat) - np.dot(Be, Bhat)))
+
+        # length along the 'B' track.  Within the track is s in [0, 1]
+        s = 1./det*(np.dot(Ahat, Ahat)*(np.dot(Ae, Bhat) - np.dot(Be, Bhat)) + np.dot(Ahat, Bhat)*(np.dot(Be, Ahat) - np.dot(Ae, Ahat)))
+
+        if l < 0:
+            pocaA = Ae
+        elif l > 1:
+            pocaA = As
+        else:
+            pocaA = Ae + l*Ahat
+            
+        if s < 0:
+            pocaB = Be
+        elif s > 1:
+            pocaB = Bs
+        else:
+            pocaB = Be + s*Bhat
+
+        return np.power(np.dot(pocaA, pocaB), 0.5)
+            
     else:
         raise ValueError ("this metric is not defined!")
 
