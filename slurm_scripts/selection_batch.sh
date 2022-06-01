f4f134965fb4bd8a87649d5af49ffae9bef0dc92
Daniel Douglas <dougl215@msu.edu>
2022-06-01 10:21:11 -0700

    tweaking slurm parameters.  Now works!

--- slurm_scripts/selection_batch.sh
+++ slurm_scripts/selection_batch.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=module0_crs_sel
+#SBATCH --job-name=m0_US_sel
 #SBATCH --output=output-$1-%j.txt
 #SBATCH --error=output-$1-%j.txt
 #
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=5g
 #SBATCH --constraint=haswell
 
 PYTHON_EXEC=/global/homes/d/ddouglas/.conda/envs/larnd-sim-dev/bin/python3
