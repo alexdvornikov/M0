5b6743f404d762a06050443f2a3fb5456d284f7a
Alex Dvornikov <olexiy@hawaii.edu>
2022-06-09 14:49:49 -1000

    Personalized slurm script.

--- slurm_scripts/selection_batch.sh
+++ slurm_scripts/selection_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=m0_US_sel
-#SBATCH --output=output-$1-%j.txt
-#SBATCH --error=output-$1-%j.txt
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
 #
 #SBATCH --constraint=haswell
 #SBATCH --account=dune
@@ -10,8 +10,8 @@
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-PYTHON_EXEC=/global/homes/d/ddouglas/.conda/envs/larnd-sim-dev/bin/python3
-SCRIPTS_PATH=/global/project/projectdirs/dune/users/ddouglas/M0
+PYTHON_EXEC=/global/common/software/nersc/cori-2022q1/sw/python/3.9-anaconda-2021.11/bin/python3
+SCRIPTS_PATH=/global/project/projectdirs/dune/users/olexiy/M0
 GEOMETRY_FILE=$SCRIPTS_PATH/pixel_layouts/multi_tile_layout-2.3.16.yaml
 DETECTOR_FILE=$SCRIPTS_PATH/detector_properties/module0.yaml
 
