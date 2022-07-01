4e14daff98ac2b5fd8a424efecf13a75eea6e23c
Daniel Douglas <dougl215@msu.edu>
2022-07-01 12:50:12 -0400

    added slurm scripts for closeness measurements

---
+++ slurm_scripts/closeness_batch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=m0_US_sel
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+#
+#SBATCH --constraint=haswell
+#SBATCH --account=dune
+#SBATCH --qos=shared
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+PYTHON_EXEC=/global/common/software/nersc/cori-2022q1/sw/python/3.9-anaconda-2021.11/bin/python3
+SCRIPTS_PATH=/global/project/projectdirs/dune/users/dougl215/M0
+GEOMETRY_FILE=$SCRIPTS_PATH/pixel_layouts/multi_tile_layout-2.3.16.yaml
+DETECTOR_FILE=$SCRIPTS_PATH/detector_properties/module0.yaml
+
+INFILE1=$1
+INFILE2=$2
+OUTFILE=$3
+
+COMMAND="$PYTHON_EXEC $SCRIPTS_PATH/track_closeness.py $INFILE1 $INFILE2 -o $OUTFILE"
+
+$COMMAND
---
+++ slurm_scripts/closeness_batch_manager.py
@@ -0,0 +1,86 @@
+# python3 selection_batch_manager.py -i /global/project/projectdirs/dune/www/data/Module0/TPC1+2/dataRuns/tracksData -r /global/project/projectdirs/dune/www/data/Module0/runlist.txt -o /global/project/projectdirs/dune/users/olexiy/M0/crossers/upstream -c upstream -n 5
+
+import os
+import argparse
+
+def main(indir, runlist, nfiles, outdir):
+    print ("input directory: " + indir)
+    print ("runlist file: " + runlist)
+    print ("cut string: " + cut)
+    print ("to be processed: " + str(nfiles))
+    print ("output directory: " + outdir)
+
+    print ("reading runlist...")
+    with open(runlist) as runlistText:
+        lines = runlistText.read().split('\n')
+        runMetaData = [{"e_field": float(line.split(' ')[0]),
+                        "charge_filename": line.split(' ')[1],
+                        "light_filename": line.split(' ')[2],
+                        "charge_thresholds": line.split(' ')[3],
+                        "light_samples": int(line.split(' ')[4])}
+                       for line in lines[1:] if line]
+
+    runConditions = {"e_field": 500,
+                     "charge_thresholds": "high"}
+
+    goodRuns = []
+    
+    if nJobs < 0:
+        nJobs = 999999999999
+        
+    n_launched = 0
+    for thisRun in runMetaData:
+        conditions_met = [thisRun[key] == value
+                          for key, value in runConditions.items()]
+        if all(conditions_met):
+            goodRuns.append(thisRun)
+
+    for thisRunA in goodRuns:
+        for thisRunB in goodRuns:
+            if hash(thisRunA) >= hash(thisRunB):
+                if n_launched < nJobs:
+                    
+                    rel_infilenameA = 'datalog_'+thisRunA['charge_filename']+'evd.h5'
+                    infileNameA = os.path.join(indir, rel_infilenameA)
+
+                    rel_infilenameB = 'datalog_'+thisRunB['charge_filename']+'evd.h5'
+                    infileNameB = os.path.join(indir, rel_infilenameB)
+                    
+                    rel_outfileName = '_'.join(['crossing',
+                                                thisRunA['charge_filename'],
+                                                thisRunB['charge_filename'],
+                                                '.npy'])
+                    outfileName = os.path.join(outdir, rel_outfileName)
+
+                    sbatch_cmd = " ".join(["sbatch ./closeness_batch.sh",
+                                           infileNameA,
+                                           infileNameB,
+                                           outfileName])
+                    os.system(sbatch_cmd)
+                    
+                    n_launched += 1
+
+                    print ("input: " + infileName)
+                    print ("output: " + outfileName)
+                    print ("sbatch command: " + sbatch_cmd)
+                    print ()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--indir','-i',
+			required=True,
+			type=str)
+    parser.add_argument('--runlist','-r',
+			required=True,
+			type=str)
+    parser.add_argument('--nfiles','-n',
+			default = -1,
+			type=int)
+    parser.add_argument('--outdir','-o',
+			required=True,
+			type=str)
+
+    args = parser.parse_args()
+
+    main(**vars(args))
