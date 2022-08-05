27f0be9c473852f71a37f05352930be1db3bc539
Daniel Douglas <dougl215@msu.edu>
2022-08-05 14:19:48 -0400

    added fancy indexing and projection.  Y scanning needs a work

--- cathode_crossing_volume_ana.py
+++ cathode_crossing_volume_ana.py
@@ -2,20 +2,41 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-bins = (np.linspace(-300, 300, 31),
-        np.linspace(-800, 380, 61),
-        np.linspace(-300, 300, 31),
-        )
+projection = 'x'
+scanDir = 'y'
 
-denom_hist = np.zeros(shape = [len(bins[0])-1,
-                               len(bins[1])-1,
-                               len(bins[2])-1])
-dx_hist = np.zeros(shape = [len(bins[0])-1,
-                            len(bins[1])-1,
-                            len(bins[2])-1])
+if scanDir == 'x':
+    plotAx = ('z', 'y')
+elif scanDir == 'y':
+    plotAx = ('x', 'z')
+elif scanDir == 'z':
+    plotAx = ('x', 'y')
+
+sigma_axial = 1.
+# sigma_axial = 1.
+sigma_radial = 1.
+
+unit_vectors = {'x': np.array([1, 0, 0]),
+                'y': np.array([0, 1, 0]),
+                'z': np.array([0, 0, 1]),
+                }
+
+bins = {'x': np.linspace(-300, 300, 31),
+        'y': np.linspace(-800, 380, 61),
+        'z': np.linspace(-300, 300, 31),
+        }
+
+histkwargs = {'bins': (bins['x'], bins['y'], bins['z'])}
+
+denom_hist = np.zeros(shape = [len(bins['x'])-1,
+                               len(bins['y'])-1,
+                               len(bins['z'])-1])
+disp_hist = np.zeros(shape = [len(bins['x'])-1,
+                              len(bins['y'])-1,
+                              len(bins['z'])-1])
 
 inputDir = './cathode_crossers_volumetric'
-for infile in tqdm(os.listdir(inputDir)):
+for infile in tqdm(os.listdir(inputDir)[:10]):
     path = os.path.join(inputDir, infile)
 
     true = np.load(path)[0]
@@ -23,39 +44,59 @@ for infile in tqdm(os.listdir(inputDir)):
     pca_dir = np.load(path)[2]
     if np.any(reco):
         ds = np.diff(np.array([true, reco]) , axis = 0)[0]
+        sigma = np.sqrt(np.power(np.dot(pca_dir,
+                                        unit_vectors[projection])
+                                 * sigma_axial, 2)
+                        + np.power(np.linalg.norm(np.cross(pca_dir,
+                                                           unit_vectors[projection]),
+                                                  axis = -1)
+                                   * sigma_radial, 2))
+
+        displacement_proj = np.dot(ds, unit_vectors[projection])
 
         denom_hist += np.histogramdd(reco,
-                                     bins = bins)[0]
-        dx_hist += np.histogramdd(reco,
-                                  weights = ds[:,0],
-                                  bins = bins)[0]
+                                     weights = 1./sigma,
+                                     **histkwargs)[0]
+        disp_hist += np.histogramdd(reco,
+                                    weights = displacement_proj/sigma,
+                                    **histkwargs)[0]
 
-dx_mean = dx_hist/denom_hist
+disp_mean = disp_hist/denom_hist
 
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
-nonNaN = dx_mean[~np.isnan(dx_mean)]
-print(nonNaN)
+nonNaN = disp_mean[~np.isnan(disp_mean)]
 vext = np.max([np.max(nonNaN), -np.min(nonNaN)])
-zBinCenters = 0.5*(bins[2][1:] + bins[2][:-1])
-for i, binCenter in enumerate(zBinCenters):
-    fig = plt.figure()
-    plt.imshow(dx_mean[:,:,i].T,
+
+scanBinCenters = 0.5*(bins[scanDir][1:] + bins[scanDir][:-1])
+for i, binCenter in enumerate(scanBinCenters):
+    # fig = plt.figure()
+    if scanDir == 'x':
+        thisSlice = disp_mean[i,:,:]
+    elif scanDir == 'y':
+        thisSlice = disp_mean[:,i,:].T
+    elif scanDir == 'z':
+        thisSlice = disp_mean[:,:,i].T
+
+    plt.imshow(thisSlice,
                origin = 'lower',
-               extent = (np.min(bins[0]), np.max(bins[0]),
-                         np.min(bins[1]), np.max(bins[1])),
+               extent = (np.min(bins[plotAx[0]]),
+                         np.max(bins[plotAx[0]]),
+                         np.min(bins[plotAx[1]]),
+                         np.max(bins[plotAx[1]])),
                cmap = 'RdYlBu',
                vmin = vext,
                vmax = -vext)
-    plt.xlabel(r'x [mm]')
-    plt.ylabel(r'y [mm]')
-    plt.title(r'z = '+str(binCenter)+ r' mm')
+    plt.xlabel(plotAx[0]+r' [mm]')
+    plt.ylabel(plotAx[1]+r' [mm]')
+    plt.title(scanDir+r' = '+str(binCenter)+ r' mm')
     plt.gca().set_aspect('equal')
     cb = plt.colorbar()
-    cb.set_label(r'$\Delta x$ [mm]')
+    cb.set_label(r'$\Delta '+projection+r'$ [mm]')
     plt.subplots_adjust(left=-0.9, bottom=0.125, right=1, top=0.9)
 
-    # plt.savefig('cathode_crosser_volume_plots/slice_z_'+str(binCenter)+'.png',
-    #             dpi=300)
-    plt.show()
+    plt.savefig('cathode_crosser_volume_plots/delta_'+projection+'_slice_'+scanDir+'_'+str(binCenter)+'.png',
+                dpi=300)
+    # plt.show()
+    plt.clf()
